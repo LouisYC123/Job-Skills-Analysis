d167ad21c1580d12d75c201453a86cda5993878b
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-04-23 20:16:47 +1000

    main now pulls keys from env vars

--- get_data/src/main.py
+++ get_data/src/main.py
@@ -1,16 +1,22 @@
-# %%
-from extract import get_query_params, query_jobs_from_api, create_jobs_json, api_targets
+from extract import get_query_params, query_jobs_from_api, api_targets
 from modules.s3 import get_s3_client, save_json_to_s3
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-now = datetime.now()
+load_dotenv()
 
-current_time = now.strftime("%Y%m%d-%H%M%S")
 
 SEARCH_TERM = "data engineer"
 TARGET_COUNTRIES = api_targets.COUNTRIES_LIST
-MAX_NUM_PAGES_PER_COUNTRY = 1
+MAX_NUM_PAGES_PER_COUNTRY = 2
+API_KEY = os.getenv("API_KEY")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+BUCKET_NAME = "lg-job-skills-data-lake"
+PREFIX_NAME = "raw_jobs_data"
 
+current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 if __name__ == "__main__":
     # Build the query params dict
@@ -18,33 +24,18 @@ if __name__ == "__main__":
         api_key=API_KEY, search_term=SEARCH_TERM, search_locations=TARGET_COUNTRIES
     )
     # Query the google_jobs engine
-    jobs_list = query_jobs_from_api(
+    jobs_dict = query_jobs_from_api(
         max_pages_per_country=MAX_NUM_PAGES_PER_COUNTRY,
         query_params=params,
     )
-    # Convert list of dictionaries to single json string
-    jobs_json = create_jobs_json(jobs_list)
-
     # Load json to s3
     s3 = get_s3_client(
         aws_access_key=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
     save_json_to_s3(
         s3,
-        bucket_name="lg-job-skills-data-lake",
-        json_object=jobs_json,
+        bucket_name=BUCKET_NAME,
+        prefix_name=PREFIX_NAME,
+        data=jobs_dict,
         timestamp=current_time,
     )
-
-    # temp
-    # ? Set up a cache??
-    # with open("jobs.json", "w", encoding="utf-8") as f:
-    #     json.dump(
-    #         {result["job_id"]: result for result in jobs_list},
-    #         f,
-    #         ensure_ascii=False,
-    #         indent=4,
-    #     )
-    print("complete")
-
-# %%
