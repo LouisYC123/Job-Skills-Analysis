fa51257b4ae4512deecebf7738ca97da5d0706ad
LouisYC123 <97873724+LouisYC123@users.noreply.github.com>
2023-07-02 16:00:11 +1000

    added run_scrapy_spider()

--- dags/dag_indeed_jobs.py
+++ dags/dag_indeed_jobs.py
@@ -1,14 +1,13 @@
 from airflow.decorators import dag, task
 from airflow.operators.python_operator import PythonOperator
 from scrapy.crawler import CrawlerProcess
-from webscrapers.indeed_scraper.indeed_spider.indeed_scrapy_spider.spiders.indeed_uk import IndeedJobSpider
+from webscrapers.indeed_scraper.indeed_scrapy_spider.spiders.indeed_uk import (
+    IndeedJobSpider,
+)
+from scrapy.utils.project import get_project_settings
 from datetime import datetime, timedelta
 import pendulum
 
-def run_scrapy_spider():
-    process = CrawlerProcess()
-    process.crawl(YourSpider)
-    process.start()
 
 # -------------- CONFIG
 DAG_VERSION = "0.0.1"
@@ -21,9 +20,10 @@ default_args = {
     "provide_context": True,
 }
 
+
 @dag(
     dag_id=f"dag_indeed_scrape_v{DAG_VERSION}",
-    start_date=datetime(2023, 6, 25),
+    start_date=datetime(2023, 7, 2),
     schedule_interval=None,
     # schedule_interval="@weekly",  #  once a week at midnight on Sunday morning
     default_args=default_args,
@@ -31,9 +31,19 @@ default_args = {
     dagrun_timeout=timedelta(hours=1),
     tags=["testing"],
 )
+def dag():
+    @task.python()
+    def run_scrapy_spider():
+        process = CrawlerProcess(
+            settings={
+                "FEED_URI": "s3://lg-job-skills-data-lake/raw_jobs_data/indeed_jobs/test_(time).json",
+                "FEED_FORMAT": "json",
+            }
+        )
+        process.crawl(IndeedJobSpider)
+        process.start()
 
-scrapy_task = PythonOperator(
-    task_id='scrapy_task',
-    python_callable=run_scrapy_spider,
-    dag=dag
-)
+    run_scrapy_spider()
+
+
+dag()
